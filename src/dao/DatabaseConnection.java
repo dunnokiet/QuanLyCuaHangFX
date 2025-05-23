bf253a8d7f72ace87d25f102d6dccc07c9480feb
dunnokiet <tianhaivn@gmail.com>
2025-05-24 01:57:01 +0700

    chore: init project

---
+++ src/dao/DatabaseConnection.java
@@ -0,0 +1,15 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=QLNH;trustServerCertificate=true";
+    private static final String USER = "sa";
+    private static final String PASSWORD = "sa";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
