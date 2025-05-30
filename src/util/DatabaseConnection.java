eeb6132a76cdd502be2c07b2959ec338fe2fbf27
dunnokiet <tianhaivn@gmail.com>
2025-05-31 04:13:23 +0700

    chore: đổi TrangChu và SanPham sang temp

--- src/util/DatabaseConnection.java
+++ src/util/DatabaseConnection.java
@@ -3,13 +3,32 @@ package util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=QLCH;trustServerCertificate=true";
-    private static final String USER = "sa";
-    private static final String PASSWORD = "sa";
+    private static final String PROPERTIES_FILE = "db.properties";
+
+    private static String url;
+    private static String user;
+    private static String password;
+
+    static {
+        try (FileInputStream input = new FileInputStream(PROPERTIES_FILE)) {
+            Properties prop = new Properties();
+            prop.load(input);
+
+            url = prop.getProperty("db.url");
+            user = prop.getProperty("db.user");
+            password = prop.getProperty("db.password");
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        return DriverManager.getConnection(url, user, password);
     }
 }
